7da939113622ff58a45f2903ac81eba6dfda5c36
kor0chka <93658238+kor0chka@users.noreply.github.com>
2021-11-21 22:20:37 +0600

    Add files via upload

---
+++ Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="wrapper">
+
+    <header>
+      <div class="navbar">
+        <div class="container">
+          <div class="navbar-content">
+            <div class="logo"> Users CRM </div>
+            <ul class="navbar-list">
+              <li class="navbar-item" v-for="link in links" :key="link.title">
+                <router-link class="mavbar-link" :title="link.title" :to="link.url">{{}}</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      links: [
+        { title: 'Home', url: '/'},
+        { title: 'Users', url: '/users'}
+      ]
+    }
+  }
+}
+</script>
